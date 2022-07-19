4794d71ba81c10aeacdae9e09ea3eac351ae811f
Patrick Hall <pathall@gmail.com>
2022-07-18 20:52:44 -0400

    use md-block component

--- js/docling-land.js
+++ js/docling-land.js
@@ -1,3 +1,6 @@
 export {SiteNav} from './SiteNav.js'
 export {LessonIndex} from '../lessons/LessonIndex.js'
+export {SiteHeader} from '../js/SiteHeader.js'
+export {MarkdownBlock, MarkdownSpan, MarkdownElement} from "/js/md-block.js";
+
 
