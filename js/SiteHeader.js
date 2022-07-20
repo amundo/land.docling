72b95af98a311ce693de14a34a675d247e00ac9e
Patrick Hall <pathall@gmail.com>
2022-07-19 21:31:37 -0400

    fix site-header

--- js/SiteHeader.js
+++ js/SiteHeader.js
@@ -3,19 +3,20 @@ export class SiteHeader extends HTMLElement {
     super()
     this.innerHTML = `
     <h1>
-      <a href="lessons.html">
+      <a href="/lessons.html">
         <span class="handwritten">docling</span><span class="digitized">.land</span>
       </a>
     </h1>
     <h2></h2>
+    <site-nav></site-nav>
     `
     
     this.render()
   }
-
+  
   render(){
+    document.title = this.dataset.title
     this.querySelector("h2").textContent = this.dataset.title
-
   }
 }
 
